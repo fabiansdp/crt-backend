2210c177f97e0c9c2426804024afb135b4dd5cf1
Fabian <13519140@std.stei.itb.ac.id>
2021-06-02 13:24:20 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8080; // default port to listen
+
+app.use(cors());
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
